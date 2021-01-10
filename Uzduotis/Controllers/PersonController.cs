e26240d067b37c7169d159a84b7fb1783732b4c7
taudau1 <37015042+taudau1@users.noreply.github.com>
2021-01-10 02:13:56 +0100

    Add files via upload

---
+++ Uzduotis/Controllers/PersonController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Uzduotis.Services;
+
+namespace Uzduotis.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PersonController : ControllerBase
+    {
+        private readonly IPersonData db;
+
+        public PersonController(IPersonData db)
+        {
+            this.db = db;
+        }
+
+        [HttpGet]
+        public List<Person> GetAll()
+        {
+            return db.GetAll().ToList();
+        }
+    }
+}
---
+++ Uzduotis/Objects/Person.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Uzduotis
+{
+    public class Person
+    {
+        public int ID { get; set; }
+        public string Vardas { get; set; }
+        public string Pavarde { get; set; }
+        public string Telefonas { get; set; }
+        public string ElPastas { get; set; }
+    }
+}
